8300486a343e5119fd41cb5be1f2c06d7da7bb24
Risha Sringa Changmai <michael.the.don@gmail.com>
2021-09-29 01:40:06 +0530

    updated create customer

--- src/api/controllers/customers/customers.controller.ts
+++ src/api/controllers/customers/customers.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { CustomersService } from './customers.service';
 import { CreateCustomerDto } from './dto/create-customer.dto';
 import { UpdateCustomerDto } from './dto/update-customer.dto';
-import { Ctx } from "../../common/decorator/request-context.decorator";
-import { RequestContext } from "../../common/RequestContext/request-context";
+import { Ctx } from '../../common/decorator/request-context.decorator';
+import { RequestContext } from '../../common/RequestContext/request-context';
 
 @Controller('customers')
 export class CustomersController {
@@ -28,7 +36,10 @@ export class CustomersController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCustomerDto: UpdateCustomerDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateCustomerDto: UpdateCustomerDto,
+  ) {
     return this.customersService.update(+id, updateCustomerDto);
   }
 
