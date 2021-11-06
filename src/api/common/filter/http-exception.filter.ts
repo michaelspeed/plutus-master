c5b84df34430dbef229b6894cee8d6599e95fde1
Risha Sringa Changmai <michael.the.don@gmail.com>
2021-11-06 15:49:10 +0530

    updated get one and update

--- src/api/common/filter/http-exception.filter.ts
+++ src/api/common/filter/http-exception.filter.ts
@@ -13,7 +13,7 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
     const status = exception.getStatus();
-    console.log(status)
+    console.log(exception);
     response.status(status).json({
       statusCode: status,
       message: exception.message,
