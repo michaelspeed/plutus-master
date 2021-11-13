98fc301f64f33347563cdff4b0073c5aad54a5e1
Risha Sringa Changmai <michael.the.don@gmail.com>
2021-11-13 23:12:35 +0530

    some update

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
     .setDescription('Api for plutus')
     .setVersion('1.0')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.enableCors();
