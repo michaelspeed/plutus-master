b7c69f10b8f8aeb863f868752901cb7c5c59b291
Risha Sringa Changmai <michael.the.don@gmail.com>
2021-11-14 12:41:57 +0530

    updated products controller

--- src/api/controllers/products/dto/create-product.dto.ts
+++ src/api/controllers/products/dto/create-product.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { IsBoolean, IsNumber, IsString } from "class-validator";
-import { Type } from "class-transformer";
+import { IsBoolean, IsNumber, IsOptional, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class CreateProductDto {
   @ApiProperty()
@@ -9,46 +9,57 @@ export class CreateProductDto {
 
   @ApiPropertyOptional()
   @IsString()
+  @IsOptional()
   description: string;
 
   @ApiPropertyOptional()
   @IsString()
+  @IsOptional()
   category: string;
 
   @ApiPropertyOptional()
   @IsBoolean()
+  @IsOptional()
   expiry: boolean;
 
   @ApiPropertyOptional()
   @IsString()
+  @IsOptional()
   batch_no: string;
 
   @ApiPropertyOptional()
   @IsString()
+  @IsOptional()
   HSN: string;
 
   @ApiPropertyOptional()
-  @IsNumber()
+  @IsNumber({ allowNaN: true })
   @Type(() => Number)
+  @IsOptional()
   GST: number;
 
   @ApiPropertyOptional()
   @IsString()
+  @IsOptional()
   mgf: string;
 
   @ApiPropertyOptional()
   @IsString()
+  @IsOptional()
   group: string;
 
   @ApiPropertyOptional()
   @IsBoolean()
+  @IsOptional()
   tax: boolean;
 
   @ApiPropertyOptional()
   @IsBoolean()
+  @IsOptional()
   tax_free: boolean;
 
   @ApiPropertyOptional()
   @IsString()
+  @IsOptional()
   organization: string;
 }
